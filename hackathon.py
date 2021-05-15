faa32527905d89334eed66a6f6f9f492b28d7776
Sneha Sawla <snehasawla2409@gmail.com>
2021-05-15 23:30:23 +0530

    final test

--- hackathon.py
+++ hackathon.py
@@ -4,36 +4,44 @@ csv_list = list(csv_file)
 
 
 def sortByName():
-    sort_csv = sorted(csv_list[1:], key= lambda x: x[-1], reverse=True);
+    sort_csv = sorted(csv_list[1:], key= lambda x: x[-1], reverse=False);
     return sort_csv
 
-print(sortByName())
 
 def sortByInput():
-    name = input('Enter Comapany Name')
-    for row in csv_list:
-        if row[-1] == name:
-            sort_name = sorted(row, key= lambda x: x[5], reverse=False)
-            print(sort_name)
+    name = input('Enter Comapany Name: ')
+    for row in csv_file:
+        if name == row[6]:
+            sort_name = sorted(row[1:], key= lambda x:int(x[5]), reverse=True)
+            print(sort_name, end='\n')
+ 
+
 
 
 def maxCloseByCompanyDateRange():
-    name = input('Enter comapny Name')
+    name = input('Enter comapny Name: ')
     start_Date = input('starting date: ')
     end_Date = input('end date: ')
-    for row in csv_list:
+    for row in csv_file:
         if row[-1] == name:
             if row[0] >= start_Date:
                 if row[0] <= end_Date:
-                    print(max(float(row[4])))
-
-
+                    value = max(float(row[4]))
+                    if value == row[4]:
+                        return row[4]   
+                    
+ 
+print(maxCloseByCompanyDateRange()) 
 
 def voletileStockDate():
     date = input('Enter Date:')
-    for row in csv_list:
+    for row in csv_file:
         max =0
         if date == row[0]:
             if abs(int(row[4])- int(row[1])) > max:
                 max = abs(int(row[4]) - int(row[1]))
-        print(max)                
+            else:
+                max = max;              
+        if max == abs(int(row[4]) - int(row[1])):
+            return row
+
